cebfa5f9e49f6990bc3803d954eac23a06e451f0
jmauro33 <jonathanmauro16@yahoo.com>
2020-04-29 15:10:39 -0500

    adding cards to projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,10 +3,9 @@ import Card  from "./cards"
 function Projects(props) {
     return (
         
-       <div>
+    <div>
       <  div class="row row-cols-1 row-cols-md-2">
           <div class="col mb-4"></div>
-     
        <Card title = "Lost-Pet" description = "Group Project implementing Node.js, Express as well as an SQL database to track missing pets plus front end" link = ""/>
        <div class="col mb-4"></div>
        <Card title = "Fitness-Tracker" description = "Fitness app to track fitness workouts implementing Mongo-DB " link = "https://jonathanfitnesstracker.herokuapp.com/?id=5e9736d3dcba090017a5f3ee"/>
@@ -15,7 +14,19 @@ function Projects(props) {
        <div class="col mb-4"></div>
        <Card title = "project" description = "description" link = ""/>
           </div>
-        </div>             
+          <  div class="row row-cols-1 row-cols-md-2">
+        
+          <div class="col mb-4"></div>
+       <Card title = "project" description = "description" link = ""/>
+       <div class="col mb-4"></div>
+       <Card title = "project" description = "description" link = ""/>
+       <div class="col mb-4"></div>
+       <Card title = "project" description = "description" link = ""/>
+       <div class="col mb-4"></div>
+       <Card title = "project" description = "description" link = ""/>
+          </div>
+
+      </div>             
           
   );
 }
