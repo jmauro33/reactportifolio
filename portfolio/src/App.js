76b720538a75a46b7efee92dd40ec9c0d97158dd
jmauro33 <jonathanmauro16@yahoo.com>
2020-04-29 10:57:55 -0500

    trying to get code from old portfolio to be compatiable

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,22 +4,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Navbar></Navbar> <div class="row">
+    <div class="col-6">
+     
     </div>
+
+    <div class="col-6">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.html">Jonathan Mauro</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="portfolio.html">portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://www.linkedin.com/in/jonathanmauro89">Linkedin</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/jmauro33">github</a>
+            </li>
+          </ul>
+      </nav>
+    </div> 
+    
   );
 }
 
---
+++ portfolio/src/about.js
@@ -0,0 +1,29 @@
+import React from "react"
+ 
+function About(props) {
+  return (
+   <About></About> <div>
+    <div class="topcontainer">
+      <div class="row">
+        <div class="col-sm-12">
+          <p>About Me</p>
+      
+          <hr>
+
+          <p>My name is Jonathan Mauro I am a graduate of the University of Central Missouri with a degree in Sports
+            Management, I have a good understanding of basic web design and coding. I plan to use what I learn from this
+            course to prepare me for any potential employment opportunity. My day job is at a health club called 68s
+            inside
+            sports, it is owned by former Chiefs player Will Shields, in addition to working there I also work at
+            Arrowhead
+            Stadium as an usher during chiefs games, as well as at Childrens Mercy Park during Sporting KC games. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+ </About>
+export default About;
