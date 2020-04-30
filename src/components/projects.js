9574925545f281833951d74e771d43eda41ba44f
jmauro33 <jonathanmauro16@yahoo.com>
2020-04-30 14:28:56 -0500

    updating with sucessful projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,16 +10,12 @@ function Projects(props) {
        <div class="col mb-4"></div>
        <Card title = "Fitness-Tracker" description = "Fitness app to track fitness workouts implementing Mongo-DB " link = "https://jonathanfitnesstracker.herokuapp.com/?id=5e9736d3dcba090017a5f3ee"/>
        <div class="col mb-4"></div>
-       <Card title = "Employee-Searcher" description = "Used React Mongo DB and API routes to creat an employee search app" link = ""/>
-       <div class="col mb-4"></div>
-       <Card title = "Budget-Tracker" description = "Budget tracker app using Webpacks and mongoDB" link = ""/>
+       <Card title = "Budget-Tracker" description = "Budget tracker app using Webpacks and mongoDB" link = " https://jonathan-budget-tracker.herokuapp.com/"/>
           </div>
 
           <  div class="row row-cols-1 row-cols-md-2">
           <div class="col mb-4"></div>
-       <Card title = "Note-Taker" description = "Note taking app using Node and express routes" link = ""/>
-       <div class="col mb-4"></div>
-       <Card title = "Drinky-Links" description = "Group Project using HTML, CSS, JS and APIs to creat a drink recipie app" link = ""/>
+       <Card title = "Note-Taker" description = "Note taking app using Node and express routes" link = " https://jmauronotes.herokuapp.com/"/>
        <div class="col mb-4"></div>
        <Card title = "Book Reviwer" description = "Upcoming Book Reviewing app assignment that will implement React, Express, Mongo and Node" link = ""/>
        <div class="col mb-4"></div>
