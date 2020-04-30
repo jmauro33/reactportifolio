65e779689ae5a974cb7615db94671a9c756e21c7
jmauro33 <jonathanmauro16@yahoo.com>
2020-04-30 17:08:05 -0500

    figuring out how to set up cards

--- src/components/cards.js
+++ src/components/cards.js
@@ -7,9 +7,10 @@ import React from 'react';
 function Cards(props) {
 
 return(
-<div className="card" style={{width:"350px",height:"250px"}}>
+<div className="card" style={{width:"350px",height:"350px"}}>
                                      
 <div className="card-body">
+    <br></br>
     <h5 className="card-title">{props.title}</h5>
     <p className="card-text">{props.description}</p>
     <a href={props.link}>{props.title}</a>
