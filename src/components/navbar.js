51096d680e6b726c243d9e5c2e77d014990444ec
jmauro33 <jonathanmauro16@yahoo.com>
2020-04-30 18:13:51 -0500

    fixing up navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,11 +8,9 @@ function Navbar() {
   return (
     <div>
       <div className="row"></div>
-  
-
         <div className="col-12">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="index.html">Jonathan Mauro</a>
+            <p>Jonathan Mauro</p>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
